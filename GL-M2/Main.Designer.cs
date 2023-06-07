9dca3e2cb41d98d80453f5c4c0800c1d61a58891
Jakkapan Attala <Jakkapan.attala@gmail.com>
2023-06-07 20:24:44 +0700

    Add project files.

---
+++ GL-M2/Main.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace GL_M2
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.testToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.settingToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1264, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // settingToolStripMenuItem
+            // 
+            this.settingToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.testToolStripMenuItem});
+            this.settingToolStripMenuItem.Name = "settingToolStripMenuItem";
+            this.settingToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.settingToolStripMenuItem.Text = "Setting";
+            // 
+            // testToolStripMenuItem
+            // 
+            this.testToolStripMenuItem.Name = "testToolStripMenuItem";
+            this.testToolStripMenuItem.Size = new System.Drawing.Size(94, 22);
+            this.testToolStripMenuItem.Text = "Test";
+            this.testToolStripMenuItem.Click += new System.EventHandler(this.testToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 659);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1264, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(5);
+            this.panel1.Size = new System.Drawing.Size(1264, 635);
+            this.panel1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(5, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1251, 622);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(1280, 720);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GL-M2";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem settingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem testToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ GL-M2/Main.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.AxHost;
+
+namespace GL_M2
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        private Bitmap bitmap = null;
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+          
+
+        }
+
+        private void DrawLine(ref Bitmap bitmap, int startX = 0, int startY = 720 , int length = 1000, int degree = 50, string colorName = "Red")
+        {
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+
+                startX = startX + 30;
+                Color color = Color.FromName(colorName);
+                // Line 1
+                using (Pen pen = new Pen(color, 1))
+                {
+                    int length2 = length;
+                    double angle = degree * Math.PI / 180; // แปลงมุมจากองศาเป็นเรเดียน
+                    int endX = startX + (int)(length2 * Math.Cos(angle));
+                    int endY = startY - (int)(length2 * Math.Sin(angle)); //
+
+                    g.DrawLine(pen, new Point(startX, startY), new Point(endX, endY));
+                }
+                // Line 2
+                using (Pen pen = new Pen(color, 1))
+                {
+                    int startX2 = bitmap.Width - startX;
+                    int startY2 = startY;
+                    double angle = (360 - degree) * Math.PI / 180; // แปลงมุมจากองศาเป็นเรเดียน
+                    int endX = startX2 - (int)(length * Math.Cos(angle));
+                    int endY = startY2 + (int)(length * Math.Sin(angle)); //
+
+                    g.DrawLine(pen, new Point(startX2, startY2), new Point(endX, endY));
+                }
+
+                // Line 3
+                using (Pen pen = new Pen(color, 1))
+                {
+                    int length1 = 200;
+                    double angle = degree * Math.PI / 180; // แปลงมุมจากองศาเป็นเรเดียน
+                    int endX = startX + (int)(length1 * Math.Cos(angle));
+                    int endY = startY - (int)(length1 * Math.Sin(angle)); //
+
+                    int startX2 = bitmap.Width - startX;
+                    int startY2 = startY;
+                    double angle2 = (360 - degree) * Math.PI / 180; // แปลงมุมจากองศาเป็นเรเดียน
+                    int endX2 = startX2 - (int)(length1 * Math.Cos(angle2));
+                    int endY2 = startY2 + (int)(length1 * Math.Sin(angle2)); //
+
+                    g.DrawLine(pen, new Point(endX, endY), new Point(endX2, endY2));
+                }
+
+                // Line 4
+                using (Pen pen = new Pen(Color.Green, 1))
+                {
+                    // 1
+                    int length1 = 365;
+                    double angle = degree * Math.PI / 180; // แปลงมุมจากองศาเป็นเรเดียน
+                    int endX = startX + (int)(length1 * Math.Cos(angle));
+                    int endY = startY - (int)(length1 * Math.Sin(angle)); //
+                    // 2
+                    int startX2 = bitmap.Width - startX;
+                    int startY2 = startY;
+                    double angle2 = (360 - degree) * Math.PI / 180; // แปลงมุมจากองศาเป็นเรเดียน
+                    int endX2 = startX2 - (int)(length1 * Math.Cos(angle2));
+                    int endY2 = startY2 + (int)(length1 * Math.Sin(angle2)); //
+
+                    g.DrawLine(pen, new Point(endX, endY), new Point(endX2, endY2));
+                }
+            }
+        }
+
+        private void DrawLineR(ref Bitmap bitmap, int startX = 0, int startY = 720, int length = 1000, int degree = 50, string colorName = "Red")
+        {
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                Color color = Color.FromName(colorName);
+                using (Pen pen = new Pen(color, 1))
+                {
+                    double angle = (360- degree) * Math.PI / 180; // แปลงมุมจากองศาเป็นเรเดียน
+                    int endX = startX - (int)(length * Math.Cos(angle));
+                    int endY = startY + (int)(length * Math.Sin(angle)); //
+
+                    g.DrawLine(pen, new Point(startX, startY), new Point(endX, endY));
+                }
+            }
+        }
+       
+
+        private bool isTimer = false;
+        private void testToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            isTimer = !isTimer;
+            if (isTimer)
+            {
+                timer1.Start();
+                toolStripStatusLabel1.Text = "Running...";
+            }
+            else
+            {
+                timer1.Stop();
+                toolStripStatusLabel1.Text = "Stop...";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bitmap?.Dispose();
+            int width = 1280;
+            int hight = 720;
+            int bottom = 10;
+            int destances = 300;
+            bitmap = new Bitmap(width, hight);
+            int degree = 70;
+
+            DrawLine(ref bitmap, destances, (hight - bottom), 500, degree: degree, colorName: "Red");
+            //DrawLine(ref bitmap, 200, 500,500, degree: 90, colorName: "Red");
+            //DrawLineR(ref bitmap, (width - destances), (hight - bottom), 200, degree: degree, colorName: "Red");
+
+            pictureBox1.Image?.Dispose();
+            pictureBox1.Image = bitmap;
+        }
+
+        #region Old
+        private void DrawCircle(ref Bitmap bitmap, Color color)
+        {
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                using (Pen pen = new Pen(color, 1))
+                {
+                    // ระบุตำแหน่งและขนาดของวงกลม
+                    // ในกรณีนี้, เราจะวาดวงกลมตรงกลางภาพ (คำนวณจากความกว้างและความสูงของ bitmap) และมีขนาดเส้นผ่าศูนย์กลาง 150 pixels
+                    int x = (bitmap.Width - 150) / 2;
+                    int y = (bitmap.Height - 150) / 2;
+                    int diameter = 150;
+
+                    // วาดวงกลม
+                    g.DrawEllipse(pen, x, y, diameter, diameter);
+                }
+            }
+        }
+        private void DrawSafetyCross(ref Bitmap bitmap, int x, int y, int size, string colorHex = "#000000")
+        {
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                // แปลงสตริง hex เป็น Color และสร้าง Pen
+                //Color color = ColorTranslator.FromHtml(colorHex);
+                Color color = Color.Green;
+                using (Pen pen = new Pen(color, 1))
+                {
+                    // คำนวณตำแหน่งของแต่ละจุด
+                    Point top = new Point(x + size / 2, y);
+                    Point bottom = new Point(x + size / 2, y + size);
+                    Point left = new Point(x, y + size / 2);
+                    Point right = new Point(x + size, y + size / 2);
+
+                    // วาดสองเส้นตรงที่แตกต่างทางที่ข้ามกัน
+                    g.DrawLine(pen, top, bottom);
+                    g.DrawLine(pen, left, right);
+                }
+            }
+        }
+        private void DrawSafetyCross(ref Bitmap bitmap, int x, int y, int size, string colorHex = "#000000", int lineWidth = 2, int gapSize = 20)
+        {
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                // แปลงสตริง hex เป็น Color และสร้าง Pen
+                //Color color = ColorTranslator.FromHtml(colorHex);
+                Color color = Color.Green;
+
+                using (Pen pen = new Pen(color, lineWidth))
+                {
+                    // คำนวณตำแหน่งของแต่ละจุด
+                    Point top1 = new Point(x + size / 2, y);
+                    Point top2 = new Point(x + size / 2, y + size / 2 - gapSize / 2);
+                    Point bottom1 = new Point(x + size / 2, y + size / 2 + gapSize / 2);
+                    Point bottom2 = new Point(x + size / 2, y + size);
+                    Point left1 = new Point(x, y + size / 2);
+                    Point left2 = new Point(x + size / 2 - gapSize / 2, y + size / 2);
+                    Point right1 = new Point(x + size / 2 + gapSize / 2, y + size / 2);
+                    Point right2 = new Point(x + size, y + size / 2);
+
+                    // วาดสี่เส้นตรงที่แตกต่างทางที่ข้ามกัน แต่ยังไม่ไปถึงจุดกึ่งกลางของ "+"
+                    g.DrawLine(pen, top1, top2);
+                    g.DrawLine(pen, bottom1, bottom2);
+                    g.DrawLine(pen, left1, left2);
+                    g.DrawLine(pen, right1, right2);
+                }
+            }
+        }
+
+#endregion
+    }
+}
