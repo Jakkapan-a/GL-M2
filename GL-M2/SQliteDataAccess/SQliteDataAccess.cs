4c3c6a1c5a11a81c5ff70bc40dcd1a2d3dc6bdbe
Jakkapan Attala <Jakkapan.attala@gmail.com>
2023-06-13 09:12:04 +0700

    Update database

--- GL-M2/SQliteDataAccess/SQliteDataAccess.cs
+++ GL-M2/SQliteDataAccess/SQliteDataAccess.cs
@@ -1,6 +1,7 @@
 ﻿using Dapper;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
 using System.Data.SQLite;
 using System.Linq;
@@ -9,54 +10,19 @@ using System.Threading.Tasks;
 
 namespace GL_M2.SQliteDataAccess
 {
-    public class SQliteDataAccess
+    public static class SQLiteDataAccess
     {
-        public static List<T> GetAll<T>(string table_name)
-        {
-            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
-            {
-                var output = cnn.Query<T>("select * from " + table_name + " order by id desc limit 100", new DynamicParameters());
-                return output.ToList();
-            }
-        }
-        // GetAllNolimit
-        public static List<T> GetAllNolimit<T>(string table_name)
-        {
-            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
-            {
-                var output = cnn.Query<T>("select * from " + table_name + " order by id desc", new DynamicParameters());
-                return output.ToList();
-            }
-        }
-
-        public static List<T> GetRow<T>(string sql)
-        {
-            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
-            {
-                var output = cnn.Query<T>(sql, new DynamicParameters());
-                return output.ToList();
-            }
-        }
-        // Update to Db
-        public static void Update(string sql, Dictionary<string, object> parameters)
-        {
-            using (IDbConnection con = new SQLiteConnection(LoadConnectionString()))
-            {
-                con.Execute(sql, parameters);
-            }
-        }
-
-        // Update to Db
-        public static void Execute(string sql, Dictionary<string, object> parameters)
-        {
-            using (IDbConnection con = new SQLiteConnection(LoadConnectionString()))
-            {
-                con.Execute(sql, parameters);
-            }
-        }
         private static string LoadConnectionString(string id = "Default")
         {
-            return "Data Source=" + System.IO.Directory.GetCurrentDirectory() + "\\" + ConfigurationManager.ConnectionStrings[id];
+            return $"Data Source={System.IO.Directory.GetCurrentDirectory()}\\{ConfigurationManager.ConnectionStrings[id]}";
+        }
+
+        public static void Execute(string sql, Dictionary<string, object> parameters)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute(sql, parameters);
+            }
         }
 
         public static string GetDateTimeNow()
@@ -64,15 +30,6 @@ namespace GL_M2.SQliteDataAccess
             return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
         }
 
-        internal static object LoadData<T>(string sql, Dictionary<string, object> parameters)
-        {
-
-            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
-            {
-                var output = cnn.Query<T>(sql, parameters);
-                return output.ToList();
-            }
-        }
         public static List<T> Query<T>(string sql, Dictionary<string, object> parameters = null)
         {
             using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
@@ -81,13 +38,15 @@ namespace GL_M2.SQliteDataAccess
                 return output.ToList();
             }
         }
-        internal static bool IsExist(string sql)
+
+        public static bool IsExist(string sql, Dictionary<string, object> parameters = null)
         {
             using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
             {
-                var output = cnn.Query(sql, new DynamicParameters());
-                return output.ToList().Count > 0;
+                var output = cnn.Query(sql, parameters).FirstOrDefault();
+                return output != null;
             }
         }
     }
+
 }
